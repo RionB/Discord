6b2bcfaed3c38382a404964aefd69a4c8ab65359
Rion Broome <rioncbroome@gmail.com>
2018-04-04 05:14:03 +0000

    extr

--- Compliment/compliment.py
+++ Compliment/compliment.py
@@ -10,10 +10,18 @@ def __init__(self, bot):
     self.bot = bot
     self.compliments = fileIO("data/Compliment/compliments.json","load")
     @commands.command(pass_context=True, no_pm=True)
-    async def compliment(self, ctx, user : discord.Member=None):
-        """Compliment the user"""
-        msg = ' ' if user != None: if user.id == self.bot.user.id: user = ctx.message.author msg = "Oh, no need for that! You go on ahead. I will stay behind, to gaze at the sun... The sun is a wondrous body. Like a magnificent father! If only I could be so grossly incandescent!"
+    async def Compliment(self, ctx, user : discord.Member=None):
+        
+        msg = ' '
+        if user != None:
+        if user.id == self.bot.user.id:
+        user = ctx.message.author 
+        msg = "Oh, no need for that! You go on ahead. I will stay behind, to gaze at the sun... The sun is a wondrous body. Like a magnificent father! If only I could be so grossly incandescent!"
         await self.bot.say(user.mention + msg) else:
         await self.bot.say(user.mention + msg + randchoice(self.compliments)) else:
         await self.bot.say(ctx.message.author.mention + msg + randchoice(self.compliments))
-        def setup(bot): bot.add_cog(Compliment(bot))
+        def setup(bot): bot.add_cog(Compliment(bot)
+    
+    def setup(bot
+    bot.add_cog(Compliment(bot))
+    

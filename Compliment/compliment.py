f27fa608e4dc1f60e6a8a0b61f23d282a1544789
Rion Broome <rioncbroome@gmail.com>
2018-04-04 04:36:25 +0000

    real files

---
+++ Compliment/compliment.py
@@ -0,0 +1,7 @@
+import discordfrom discord.ext import commands
+from .utils.dataIO import fileIO
+from random import choice as randchoice
+import os 
+
+
+class Compliment: """Rion's Compliment Cog""" def __init__(self, bot): self.bot = bot self.compliments = fileIO("data/compliment/compliments.json","load") @commands.command(pass_context=True, no_pm=True) async def compliment(self, ctx, user : discord.Member=None): """Compliment the user""" msg = ' ' if user != None: if user.id == self.bot.user.id: user = ctx.message.author msg = "Oh, no need for that! You go on ahead. I will stay behind, to gaze at the sun... The sun is a wondrous body. Like a magnificent father! If only I could be so grossly incandescent!" await self.bot.say(user.mention + msg) else: await self.bot.say(user.mention + msg + randchoice(self.compliments)) else: await self.bot.say(ctx.message.author.mention + msg + randchoice(self.compliments)) def setup(bot): bot.add_cog(Compliment(bot))
